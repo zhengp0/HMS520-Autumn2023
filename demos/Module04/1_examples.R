2b61d1ef7b9c7130c7c58b9d72acb0ddb596c1e9
Peng Zheng <zhengp@uw.edu>
2023-11-19 08:22:53 +0800

    add module04 demo

---
+++ demos/Module04/1_examples.R
@@ -0,0 +1,31 @@
+# examples
+# ========
+
+
+# scatter plot ------------------------------------------------------------
+# useful to see the general trend between dependent and independent variables
+# geom_point
+
+
+# box plot ----------------------------------------------------------------
+# inspect variable variation across different groups
+# geom_boxplot
+
+
+# bar plot ----------------------------------------------------------------
+# direct comparison of certain quantity across different groups
+# geom_bar, geom_col
+
+
+# line plot ---------------------------------------------------------------
+# usual is used for prediction
+# geom_line
+
+
+# line with uncertainty ---------------------------------------------------
+# it is pretty common to plot the CI of the fit
+# geom_ribbon
+
+
+# -------------------------------------------------------------------------
+# ?how can we fit lm for each group

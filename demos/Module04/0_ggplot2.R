aa5a7fd282f19e0ea3e8a27009e58968602a75cc
Peng Zheng <zhengp@uw.edu>
2023-11-19 10:22:11 +0800

    add demo from 20231121

--- demos/Module04/0_ggplot2.R
+++ demos/Module04/0_ggplot2.R
@@ -1,25 +1,50 @@
 # ggplot2
 # =======
+library("ggplot2")
 
 
 # data and aesthetic mappings ---------------------------------------------
+ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width, color = Species)) +
+  geom_point()
 
 
 # geometric objects --------------------------------------------------------
 # https://ggplot2.tidyverse.org/reference/
+ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width, color = Species)) +
+  geom_point() +
+  geom_line()
 
 
 # scale: scale_y_log10 ----------------------------------------------------
+ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width, color = Species)) +
+  geom_point() +
+  scale_y_log10()
 
 
 # transformation: coord_trans ---------------------------------------------
+ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width, color = Species)) +
+  geom_point() +
+  coord_trans(y = "log10")
 
 
 # polar coordinate: coord_polar -------------------------------------------
+ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width, color = Species)) +
+  geom_point() +
+  coord_polar(theta = "x")
 
 
 # facet -------------------------------------------------------------------
 # facet_wrap, facet_grid
+ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width, color = Species)) +
+  geom_point() +
+  facet_wrap(vars(Species), nrow = 3)
 
+ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width, color = Species)) +
+  geom_point() +
+  facet_grid(vars(Species), vars(Petal.Length))
 
 # save figures: ggsave ----------------------------------------------------
+fig <- ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width, color = Species)) +
+  geom_point()
+
+ggsave("graph.png", plot = fig)
--- demos/Module04/1_examples.R
+++ demos/Module04/1_examples.R
@@ -1,30 +1,63 @@
 # examples
 # ========
+library("dplyr")
+library("ggplot2")
 
 
 # scatter plot ------------------------------------------------------------
 # useful to see the general trend between dependent and independent variables
 # geom_point
+ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width, color = Species)) +
+  geom_point()
 
 
 # box plot ----------------------------------------------------------------
 # inspect variable variation across different groups
 # geom_boxplot
+ggplot(iris, aes(x = Species, y = Sepal.Length, fill = Species)) +
+  geom_boxplot()
 
 
 # bar plot ----------------------------------------------------------------
 # direct comparison of certain quantity across different groups
 # geom_bar, geom_col
+ggplot(iris, aes(x = Species)) +
+  geom_bar()
+
+ggplot(iris, aes(x = Species, y = Sepal.Length)) +
+  geom_col()
 
 
 # line plot ---------------------------------------------------------------
 # usual is used for prediction
 # geom_line
+mod <- lm(Sepal.Width ~ Sepal.Length, iris)
+print(summary(mod))
+
+iris_pred <- iris %>%
+  mutate(sepal_width_fit = predict(mod, iris))
+
+ggplot(iris_pred, aes(x = Sepal.Length)) +
+  geom_point(aes(y = Sepal.Width, color = Species)) +
+  geom_line(aes(y = sepal_width_fit))
 
 
 # line with uncertainty ---------------------------------------------------
 # it is pretty common to plot the CI of the fit
 # geom_ribbon
+
+iris_pred <- as.data.frame(predict(mod, iris, interval = "confidence")) %>%
+  rename(
+    sepal_width_fit = fit,
+    sepal_width_lwr = lwr,
+    sepal_width_upr = upr
+  ) %>%
+  bind_cols(iris)
+
+ggplot(iris_pred, aes(x = Sepal.Length)) +
+  geom_point(aes(y = Sepal.Width, color = Species)) +
+  geom_line(aes(y = sepal_width_fit)) +
+  geom_ribbon(aes(ymin = sepal_width_lwr, ymax = sepal_width_upr), alpha = 0.5)
 
 
 # -------------------------------------------------------------------------
